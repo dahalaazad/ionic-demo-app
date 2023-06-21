0d6cf144a692ff478234eeeb16ac3f588242f86e
dahalaazad <41798206+dahalaazad@users.noreply.github.com>
2023-06-21 12:28:10 +0545

    Revert "Feature/persist data"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,22 @@
-import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonList,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
-import "./Home.css";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  deleteInspiration,
-  setCurrentInspiration,
-} from "../redux/inspirationSlice";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch();
-
-  const inspirationList = useSelector(
-    (state) => state?.inspiration?.inspirationList
-  );
-  console.log(inspirationList, "inspirationList from HomePage.....");
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>HomePage</IonTitle>
+          <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-
       <IonContent fullscreen>
-        <IonList>
-          {inspirationList.map((item, index) => (
-            <div key={index}>
-              <div
-                onClick={() =>
-                  dispatch(setCurrentInspiration({ ...item, currentIndex: index }))
-                }
-              >
-                <Link to="/details" key={index}>
-                  <p>{item.title} </p>
-                </Link>
-              </div>
-
-              <IonButton onClick={() => dispatch(deleteInspiration(index))}>
-                Delete
-              </IonButton>
-            </div>
-          ))}
-        </IonList>
-
-        <Link to="/add-inspiration">
-          <IonButton fill="solid">Add Inspiration </IonButton>
-        </Link>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
